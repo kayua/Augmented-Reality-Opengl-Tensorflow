f87fbd274413ce049a59292575b50277fae2c171
kayua <kayuaolequesp@gmail.com>
2021-11-13 19:50:46 -0300

    a

---
+++ app/src/main/java/com/tensorlearning/facemasks/SpatialRecognition/SpatialRecognitionSettings.java
@@ -0,0 +1,53 @@
+package com.tensorlearning.facemasks.SpatialRecognition;
+
+
+
+public class SpatialRecognitionSettings {
+
+    private final int spatialEstimationSizeImageHeight = 256;
+    private final int spatialEstimationSizeImageWidth = 512;
+    private final int spatialEstimationCoordinateMeanImage = 128;
+    private final int spatialEstimationFlattenAllocationBuffer = spatialEstimationSizeImageHeight  * spatialEstimationSizeImageWidth;
+    private final float spatialEstimationBufferOutput = 40;
+    private final float spatialEstimationCoordinateMeanStandard = 128f;
+
+    public int getSpatialEstimationSizeImageHeight() {
+
+        return spatialEstimationSizeImageHeight;
+
+    }
+
+    public int getSpatialEstimationSizeImageWidth() {
+
+        return spatialEstimationSizeImageWidth;
+
+    }
+
+    public int getSpatialEstimationCoordinateMeanImage() {
+
+        return spatialEstimationCoordinateMeanImage;
+
+    }
+
+    public int getSpatialEstimationFlattenAllocationBuffer() {
+
+        return spatialEstimationFlattenAllocationBuffer;
+
+    }
+
+    public float getSpatialEstimationBufferOutput() {
+
+        return spatialEstimationBufferOutput;
+
+    }
+
+    public float getSpatialEstimationCoordinateMeanStandard() {
+
+        return spatialEstimationCoordinateMeanStandard;
+
+    }
+
+}
+
+
+
