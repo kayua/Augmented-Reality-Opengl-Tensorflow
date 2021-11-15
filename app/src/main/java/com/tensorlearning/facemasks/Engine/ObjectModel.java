098bfdafe7c635bfa888faab2bc1570509c653aa
kayua <kayuaolequesp@gmail.com>
2021-11-15 15:46:42 -0300

    commit

--- app/src/main/java/com/tensorlearning/facemasks/Engine/ObjectModel.java
+++ app/src/main/java/com/tensorlearning/facemasks/Engine/ObjectModel.java
@@ -5,6 +5,7 @@ import java.util.Vector;
 public final class ObjectModel {
 
     private int objectModelNumberFaces;
+    private int objectModelNumberNormal;
     private int objectModelIdObject;
     private String  objectModelMaterialReference;
 
@@ -37,15 +38,41 @@ public final class ObjectModel {
 
     }
 
+
     public void addNormalComponents(float coordinateAxisX, float coordinateAxisY, float coordinateAxisZ){
 
-
+        objectModelNormalComponents.add(coordinateAxisX);
+        objectModelNormalComponents.add(coordinateAxisY);
+        objectModelNormalComponents.add(coordinateAxisZ);
+        objectModelNumberNormal = objectModelNumberNormal + 1;
 
     }
+
+    public void addTextureComponents(float coordinateAxisX, float coordinateAxisY, float coordinateAxisZ){
+
+        objectModelTextureCoordinates.add(coordinateAxisX);
+        objectModelTextureCoordinates.add(coordinateAxisY);
+        objectModelTextureCoordinates.add(coordinateAxisZ);
+        objectModelNumberNormal = objectModelNumberNormal + 1;
+
+    }
+
+    public void addObjectPositionsComponents(float coordinateAxisX, float coordinateAxisY, float coordinateAxisZ){
+
+        objectModelObjectPositions.add(coordinateAxisX);
+        objectModelObjectPositions.add(coordinateAxisY);
+        objectModelObjectPositions.add(coordinateAxisZ);
+        objectModelNumberNormal = objectModelNumberNormal + 1;
+
+    }
+
+
+
 
     public ObjectModel() {
 
         objectModelNumberFaces = 0;
+        objectModelNumberNormal = 0;
         objectModelIdObject = 0;
         objectModelMaterialReference = "";
         objectModelNormalComponents = new Vector<>();
