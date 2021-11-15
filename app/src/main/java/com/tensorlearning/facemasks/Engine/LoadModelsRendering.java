14b935177f70472e083f0a8fd75a9e72e0dabe5e
kayua <kayuaolequesp@gmail.com>
2021-11-15 16:58:25 -0300

    commit

--- app/src/main/java/com/tensorlearning/facemasks/Engine/LoadModelsRendering.java
+++ app/src/main/java/com/tensorlearning/facemasks/Engine/LoadModelsRendering.java
@@ -2,15 +2,49 @@ package com.tensorlearning.facemasks.Engine;
 
 import android.content.Context;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public final class LoadModelsRendering {
 
+    BufferedReader reader = null;
+    Context context;
+    String fileObjectModel;
+    InputStreamReader fileInputReference;
+    String bufferReadLine;
 
 
     public LoadModelsRendering(Context context, String file) {
 
     }
 
+    public void readFileObject(){
 
+        try {
+
+            fileInputReference = new InputStreamReader(context.getAssets().open("filename.txt"));
+            reader = new BufferedReader(fileInputReference);
+
+            while ((bufferReadLine = reader.readLine()) != null) {
+
+            }
+
+        } catch (IOException e) {
+
+        } finally {
+
+            if (reader != null) {
+
+                try {
+                    reader.close();
+                } catch (IOException e) { }
+
+
+            }
+        }
+
+    }
 
 
 
